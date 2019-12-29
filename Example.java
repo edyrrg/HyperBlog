7acc005f00fbb418552c9b34ff706dae6489b45a
Edy <edyrojas64@gmail.com>
2019-12-29 14:42:26 -0600

    Nuevas funciones

--- Example.java
+++ Example.java
@@ -1,5 +1,12 @@
 public class Example {
     public static void main(String[] args){
-        System.out.println("Hola Mundo");
+        int a = 100;
+        int b = 0;
+        for(int i = 0; i < a; i++){
+            b++;
+        }
+        System.out.println("Hola Mundo" + 
+        "\n" + b );
+        System.out.println("Como estas?");
     }
 }
